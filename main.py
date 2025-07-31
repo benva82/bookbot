bf7907b557d70ec8f50c27693a9f485c22b735cf
benva82 <ben_van_asten@hotmail.com>
2025-07-31 17:50:21 +0200

    Reading text and counting words

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+def main():
+    get_book_text("books/frankenstein.txt")
+    get_word_count()
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-    print(file_contents)
+    return file_contents
 
-def main():
-    get_book_text("books/frankenstein.txt")
+def get_word_count():
+    file_contents = get_book_text("books/frankenstein.txt")
+    words_string = file_contents.split()
+    num_words = len(words_string)
+    print(f"{num_words} words found in the document")
 
 main()
